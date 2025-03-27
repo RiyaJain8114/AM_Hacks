b32c6aab82d78f3f4230d06c818d24562e78512e
Shreya-Pande <fuchchu19@gmail.com>
2025-03-27 19:18:02 +0530

    created app

---
+++ src/components/navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="sidebar">
+      <div className="logo">ResQLink Insights</div>
+      <ul className="nav-links">
+        <li><a href="#">Dashboard</a></li>
+        <li><a href="#">Live Disaster Map</a></li>
+        <li><a href="#">Alerts & Notification</a></li>
+        <li><a href="#">Emergency Requests</a></li>
+        <li><a href="#">Resource Allocation</a></li>
+        <li><a href="#">User Management (Admin only)</a></li>
+        <li><a href="#">Settings</a></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
